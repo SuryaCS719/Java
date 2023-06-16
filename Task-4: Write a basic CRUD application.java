f777bb90f0d33285ac46944bb5cb27d1f5b2d520
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-06-16 22:30:42 +0530

    Create Task-4: Write a basic CRUD application.java

---
+++ Task-4: Write a basic CRUD application.java
@@ -0,0 +1,152 @@
+// Task-4: Write a basic CRUD application
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Student {
+    private String id;
+    private String name;
+    private int age;
+
+    public Student(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + ", age=" + age + "]";
+    }
+}
+
+class CRUDApplication {
+    private List<Student> students;
+
+    public CRUDApplication() {
+        students = new ArrayList<>();
+    }
+
+    public void createStudent(Student student) {
+        students.add(student);
+        System.out.println("Student created successfully.");
+    }
+
+    public void readAllStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No students found.");
+        } else {
+            System.out.println("List of students:");
+            for (Student student : students) {
+                System.out.println(student);
+            }
+        }
+    }
+
+    public void updateStudent(String id, String newName, int newAge) {
+        for (Student student : students) {
+            if (student.getId().equals(id)) {
+                student.setName(newName);
+                student.setAge(newAge);
+                System.out.println("Student updated successfully.");
+                return;
+            }
+        }
+        System.out.println("Student not found.");
+    }
+
+    public void deleteStudent(String id) {
+        for (Student student : students) {
+            if (student.getId().equals(id)) {
+                students.remove(student);
+                System.out.println("Student deleted successfully.");
+                return;
+            }
+        }
+        System.out.println("Student not found.");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CRUDApplication application = new CRUDApplication();
+
+        while (true) {
+            System.out.println("CRUD Application");
+            System.out.println("================");
+            System.out.println("1. Create a student");
+            System.out.println("2. Read all students");
+            System.out.println("3. Update a student");
+            System.out.println("4. Delete a student");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice (1-5): ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline character
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter student ID: ");
+                    String id = scanner.nextLine();
+                    System.out.print("Enter student name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter student age: ");
+                    int age = scanner.nextInt();
+                    scanner.nextLine(); // Consume newline character
+                    Student student = new Student(id, name, age);
+                    application.createStudent(student);
+                    break;
+                case 2:
+                    application.readAllStudents();
+                    break;
+                case 3:
+                    System.out.print("Enter student ID to update: ");
+                    String updateId = scanner.nextLine();
+                    System.out.print("Enter new student name: ");
+                    String newName = scanner.nextLine();
+                    System.out.print("Enter new student age: ");
+                    int newAge = scanner.nextInt();
+                    scanner.nextLine(); // Consume newline character
+                    application.updateStudent(updateId, newName, newAge);
+                    break;
+                case 4:
+                    System.out.print("Enter student ID to delete: ");
+                    String deleteId = scanner.nextLine();
+                    application.deleteStudent(deleteId);
+                    break;
+                case 5:
+                    System.out.println("Exiting the application...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+            System.out.println();
+        }
+    }
+}
